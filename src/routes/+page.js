ab573d422302edd2ea688b8db32d55f2ea9e3dc3
Nicolas Albert <nicolas.albert@gmail.com>
2023-12-28 08:45:02 +0100

    fix compilation

--- src/routes/+page.js
+++ src/routes/+page.js
@@ -1,7 +1,17 @@
+import { browser } from '$app/environment';
+
+export const prerender = true;
+
 /** @type {import('./$types').PageLoad} */
 export async function load({ url }) {
+	if (browser) {
+		return {
+			owner: url.searchParams?.get('owner') ?? 'nicolas-albert',
+			repo: url.searchParams?.get('repo') ?? 'gmail_forwarder'
+		};
+	}
 	return {
-		owner: url.searchParams.get('owner') ?? 'nicolas-albert',
-		repo: url.searchParams.get('repo') ?? 'gmail_forwarder'
+		owner: '',
+		repo: ''
 	};
 }
